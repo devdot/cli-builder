7bdc3a7af72b81c26506af893103464049706906
Thomas Kuschan <mail@thomaskuschan.de>
2024-08-09 16:15:42 +0200

    Make printer PSR12 compatible

--- src/Generator/Printer.php
+++ src/Generator/Printer.php
@@ -8,4 +8,10 @@ class Printer extends NettePrinter
 {
     public string $indentation = '    ';
     public int $linesBetweenMethods = 1;
+    public int $linesBetweenUseTypes = 1;
+
+    protected function isBraceOnNextLine(bool $multiLine, bool $hasReturnType): bool
+    {
+        return !$multiLine;
+    }
 }
