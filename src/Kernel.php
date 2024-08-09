d60c7cf2833342a91b38810d6c08317fda4971e5
Thomas Kuschan <mail@thomaskuschan.de>
2024-08-09 10:07:22 +0200

    Initial commit

---
+++ src/Kernel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Devdot\Cli\Builder;
+
+use Devdot\Cli\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    /**
+     * @var class-string[]
+     */
+    protected array $services = [
+        //
+    ];
+
+    /**
+     * @var class-string<\Devdot\Cli\Container\ServiceProvider>[]
+     */
+    protected array $providers = [
+        //
+    ];
+
+    public function __construct(string $dir = __DIR__, string $namespace = __NAMESPACE__)
+    {
+        parent::__construct($dir, $namespace);
+    }
+}
