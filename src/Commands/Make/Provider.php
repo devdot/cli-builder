d898515fec4f9812a65ee91d9d33b98f0ce50718
Thomas Kuschan <mail@thomaskuschan.de>
2024-08-14 18:18:34 +0200

    Add make:provider

---
+++ src/Commands/Make/Provider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Devdot\Cli\Builder\Commands\Make;
+
+use Devdot\Cli\Container\ServiceProvider;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class Provider extends MakeCommand
+{
+    use ClassFromNameTrait;
+
+    protected function getMakeNamespace(): string
+    {
+        return 'Providers\\';
+    }
+
+    protected function getDefaultMakeName(): ?string
+    {
+        return null;
+    }
+
+    protected function handleBuildClass(): void
+    {
+        $class = $this->getClass();
+        $class->setExtends(ServiceProvider::class);
+        $booting = $class->addMethod('booting');
+        $booting->setReturnType('void');
+        $booting->addParameter('container')->setType(ContainerBuilder::class);
+        $booting->addBody('parent::booting($container);' . PHP_EOL . PHP_EOL . '//');
+
+        ($class->addProperty('services'))
+            ->setType('array')
+            ->setValue([])
+            ->setProtected()
+            ->addComment('@var class-string[]')
+        ;
+
+        $namespace = $this->getNamespace();
+        $namespace
+            ->addUse(ServiceProvider::class)
+            ->addUse(ContainerBuilder::class)
+        ;
+
+        // TODO: find a way to add it automatically to the kernel
+        $this->style->info('Make sure to add the provider to your Kernel!');
+    }
+}
