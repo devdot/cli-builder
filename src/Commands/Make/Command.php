0152930d61d3d63a5541b1ffb849d1496cd22fb9
Thomas Kuschan <mail@thomaskuschan.de>
2024-08-15 09:17:09 +0200

    Implement make:command

--- src/Commands/Make/Command.php
+++ src/Commands/Make/Command.php
@@ -2,11 +2,53 @@
 
 namespace Devdot\Cli\Builder\Commands\Make;
 
+use Devdot\Cli\Exceptions\CommandFailedException;
+use Symfony\Component\Console\Input\InputOption;
+
 class Command extends MakeCommand
 {
-    protected function handle(): int
-    {
+    use ClassFromNameTrait;
 
-        return self::SUCCESS;
+    const DEFAULT_BASE_COMMAND = 'Command';
+
+    protected function configure(): void
+    {
+        $this->addOption('extends', null, InputOption::VALUE_REQUIRED, 'The base Command class of your project', $this->getExtendsDefault());
+    }
+
+    protected function getMakeNamespace(): string
+    {
+        return 'Commands\\';
+    }
+
+    protected function getDefaultMakeName(): ?string
+    {
+        return null;
+    }
+
+    protected function handleBuildClass(): void
+    {
+        // find the project root base command
+        $extends = $this->input->getOption('extends');
+        if (!class_exists($extends)) {
+            throw new CommandFailedException('Base class ' . $extends . ' does not exist!');
+        }
+
+        $class = $this->getClass();
+        $class->setExtends($extends);
+
+        $class->addMethod('handle')
+            ->setProtected()
+            ->setReturnType('int')
+            ->setBody('//' . PHP_EOL . PHP_EOL . 'return self::SUCCESS;')
+        ;
+
+        $this->getNamespace()->addUse($extends);
+    }
+
+    private function getExtendsDefault(): string
+    {
+        $extends = $this->project->namespace . '\\' . $this->getMakeNamespace() . self::DEFAULT_BASE_COMMAND;
+        return class_exists($extends) ? $extends : \Devdot\Cli\Command::class;
     }
 }
